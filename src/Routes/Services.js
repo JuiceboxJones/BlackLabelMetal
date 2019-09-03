0c91977ceb0a8a58a30170b026ad93abf2786b9c
Juicebox Jones <jderhalli@gmail.com>
2019-09-03 12:55:23 -0700

    added services content

---
+++ src/Routes/Services.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class Services extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <div className='serv-over'>
+          <h1>Services Overview</h1>
+          <p>
+            Whether you have a new idea, a one off, or need to expand your production; 
+            Our team at BlackLabelMetal is ready to take care of your needs. 
+            For information on pricing give us a call, send us an email, or stop in today. 
+
+            When you've been in business as long as we have and faced technology's increasingly rapid developments, 
+            you learn a few things. Or, in our case, more than a few. We not only have simple, 
+            ready-to-implement solutions for almost every problem, we have the patents for them, too. 
+            And, if you bring us something new, our engineers would love to work with you to find the solution.
+          </p>
+        </div>
+        <div className='serv-off'>
+          <h1>What we offer</h1>
+          <ul>
+            <li>
+              <h3>Design</h3>
+              <p>
+                With the latest version of Solidworks in house, 
+                we have the ability to work with many 2 and 3 dimensional file types. 
+                Our drafting department makes use of parametric properties making for 
+                quick and easy adjustments to prototypes and change orders.
+              </p>
+            </li>
+            <li>
+              <h3>Welding</h3>
+              <p>
+                Our shop is fully equipped and set up to weld almost any variation of stainless, 
+                aluminum, and steel. Our master welders can produce multiple styles and variations 
+                of beads to best fit the clients desired outcome.
+              </p>
+            </li>
+            <li>
+              <h3>Forming</h3>
+              <p>
+                A powerful 80 ton Amada 8 Axis cnc pressbrake stands at the heart of our shop ready 
+                to make precise bends on our clients formed products. With a wide variety of tooling 
+                and our skilled operator, Black Label Metal is your go to for formed parts
+              </p>
+            </li>
+            <li>
+              <h3>Powder Coating</h3>
+              <p>
+              Every final product needs a finish, whether its clear coated steel for an industrial look, 
+              neutral display colors to emphasize a product, or bright bold attention grabbers, 
+              we offer state of the art powder coating ready to meet our clients needs​
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+     );
+  }
+}
+ 
+export default Services;
